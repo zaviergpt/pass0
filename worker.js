6ec1d4f89838666c221d784dbdbf4107b64cea52
Zavier Chiam <60832719+zaviergpt@users.noreply.github.com>
2025-02-27 17:38:53 +0800

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'pass0';
+const urlsToCache = [
+  './',
+  './index.html',
+  './icon192.png',
+  './icon512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
